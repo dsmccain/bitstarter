9a11dfcefdf41e4f62ef3aa6344a488a3356a7d7
Daniel McCain <danielmctor@gmail.com>
2013-07-09 03:55:21 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var filesys = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buffer = new Buffer(filesys.readFileSync("index.html"));
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
